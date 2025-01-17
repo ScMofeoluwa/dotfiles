3d72d8fe6b89aa37b72a781dc4bee9a65937fabc
Mofeoluwa Lijadu <lijsamobafunmilayo@gmail.com>
2025-01-17 20:02:00 +0100

    fixed issue with golsp config

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -9,66 +9,21 @@ return {
       "leoluz/nvim-dap-go",
     },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      -- Configure treesitter
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "go" },
-      })
-
-      require("go").setup({
-        capabilities = capabilities,
-        lsp_on_attach = function(_, bufnr)
-          local nmap = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-          end
-
-          nmap("<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", "Goto Definition in Vertical Split")
+      require("go").setup()
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require("go.format").goimport()
         end,
-        lsp_cfg = {
-          settings = {
-            gopls = {
-              gofumpt = true,
-              codelenses = {
-                gc_details = false,
-                generate = true,
-                regenerate_cgo = true,
-                run_govulncheck = true,
-                test = true,
-                tidy = true,
-                upgrade_dependency = true,
-                vendor = true,
-              },
-              hints = {
-                assignVariableTypes = false,
-                compositeLiteralFields = false,
-                compositeLiteralTypes = false,
-                constantValues = false,
-                functionTypeParameters = false,
-                parameterNames = false,
-                rangeVariableTypes = false,
-              },
-              analyses = {
-                fieldalignment = true,
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-              },
-              usePlaceholders = true,
-              completeUnimported = true,
-              staticcheck = true,
-              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-              semanticTokens = false,
-            },
-          },
-        },
-        luasnip = true,
-        trouble = true,
+        group = format_sync_grp,
       })
     end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
     build = ':lua require("go.install").update_all_sync()',
+    keys = {
+      { "<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", desc = "Goto Definition in Vertical Split" },
+    },
   },
 }
